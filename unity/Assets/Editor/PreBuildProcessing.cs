d425d186b6bf440be8ad24eb6f9df5287019ae37
Shengchen Zhang <hello@shengchen.io>
2025-08-31 20:05:28 +0800

    Update Python to Python2 to enable building with compression.

--- unity/Assets/Editor/PreBuildProcessing.cs
+++ unity/Assets/Editor/PreBuildProcessing.cs
@@ -4,12 +4,10 @@ using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 
-public class PreBuildProcessing : IPreprocessBuildWithReport
-{
+public class PreBuildProcessing : IPreprocessBuildWithReport {
     public int callbackOrder => 1;
-    public void OnPreprocessBuild(BuildReport report)
-    {
-        System.Environment.SetEnvironmentVariable("EMSDK_PYTHON", "/usr/bin/python3");
+    public void OnPreprocessBuild(BuildReport report) {
+        System.Environment.SetEnvironmentVariable("EMSDK_PYTHON", "/usr/local/bin/python");
     }
 }
 #endif
